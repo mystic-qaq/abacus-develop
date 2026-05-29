a9fee470f885f1cf60e1c6b2352169ea3babebc3
mystic-qaq <695724023@qq.com>
2026-05-29 15:56:56 +0800

    feat(pw): use nonblocking peer exchanges for gather/scatter

--- source/source_basis/module_pw/pw_gatherscatter.h
+++ source/source_basis/module_pw/pw_gatherscatter.h
@@ -2,6 +2,7 @@
 #include "source_base/global_function.h"
 #include "source_base/timer.h"
 #include <typeinfo>
+#include <vector>
 
 namespace ModulePW
 {
@@ -47,13 +48,21 @@ void PW_Basis::gatherp_scatters(std::complex<T>* in, std::complex<T>* out) const
     const int nstot_gps = this->nstot;
     const int nplane_gps = this->nplane;
     const int* istot2ixy_gps = this->istot2ixy;
+    const int* numg_gps = this->numg;
+    const int* numr_gps = this->numr;
+    const int* startg_gps = this->startg;
+    const int* startr_gps = this->startr;
+    const int poolrank_gps = this->poolrank;
+    const int poolnproc_gps = this->poolnproc;
+    const int send_count_gps = startr_gps[poolnproc_gps - 1] + numr_gps[poolnproc_gps - 1];
+    std::complex<T>* sendbuf = this->acquire_comm_sendbuf<T>(send_count_gps);
 #ifdef _OPENMP
     #pragma omp parallel for
 #endif
     for (int istot = 0; istot < nstot_gps; ++istot)
     {
         int ixy = istot2ixy_gps[istot];
-        std::complex<T> *outp = &out[istot * nplane_gps];
+        std::complex<T> *outp = &sendbuf[istot * nplane_gps];
         std::complex<T> *inp = &in[ixy * nplane_gps];
         for (int iz = 0; iz < nplane_gps; ++iz)
         {
@@ -64,48 +73,109 @@ void PW_Basis::gatherp_scatters(std::complex<T>* in, std::complex<T>* out) const
 
     //exchange data
     //(nplane,nstot) to (numz[ip],ns, poolnproc)
-    ModuleBase::timer::start(this->classname, "gatherp_alltoallv");
+    MPI_Datatype mpi_type = MPI_DATATYPE_NULL;
     if(typeid(T) == typeid(double))
     {
-        MPI_Alltoallv(out, numr, startr, MPI_DOUBLE_COMPLEX, in, numg, startg, MPI_DOUBLE_COMPLEX, this->pool_world);
+        mpi_type = MPI_DOUBLE_COMPLEX;
     }
     else if(typeid(T) == typeid(float))
     {
-        MPI_Alltoallv(out, numr, startr, MPI_COMPLEX, in, numg, startg, MPI_COMPLEX, this->pool_world);
+        mpi_type = MPI_COMPLEX;
     }
     else
     {
-        ModuleBase::WARNING_QUIT("PW_Basis::gatherp_scatters", "Unsupported data type for MPI_Alltoallv");
+        ModuleBase::WARNING_QUIT("PW_Basis::gatherp_scatters", "Unsupported data type for MPI gather/scatter");
+    }
+    std::vector<MPI_Request> recv_requests(poolnproc_gps, MPI_REQUEST_NULL);
+    std::vector<MPI_Request> send_requests(poolnproc_gps, MPI_REQUEST_NULL);
+    std::vector<MPI_Status> recv_status(poolnproc_gps);
+    std::vector<int> recv_indices(poolnproc_gps, MPI_UNDEFINED);
+    int active_recvs = 0;
+    int active_sends = 0;
+
+    ModuleBase::timer::start(this->classname, "gatherp_alltoallv");
+    for (int ip = 0; ip < poolnproc_gps; ++ip)
+    {
+        if (ip == poolrank_gps || numg_gps[ip] == 0)
+        {
+            continue;
+        }
+        MPI_Irecv(&in[startg_gps[ip]], numg_gps[ip], mpi_type, ip, 0, this->pool_world, &recv_requests[ip]);
+        ++active_recvs;
+    }
+    for (int ip = 0; ip < poolnproc_gps; ++ip)
+    {
+        if (ip == poolrank_gps || numr_gps[ip] == 0)
+        {
+            continue;
+        }
+        MPI_Isend(&sendbuf[startr_gps[ip]], numr_gps[ip], mpi_type, ip, 0, this->pool_world, &send_requests[ip]);
+        ++active_sends;
     }
     ModuleBase::timer::end(this->classname, "gatherp_alltoallv");
 
     // change (nz,ns) to (numz[ip],ns, poolnproc)
-    ModuleBase::timer::start(this->classname, "gatherp_unpack");
-    const int poolnproc_gps = this->poolnproc;
     const int nst_gps = this->nst;
     const int nz_gps = this->nz;
     const int* numz_gps = this->numz;
-    const int* startg_gps = this->startg;
     const int* startz_gps = this->startz;
-#ifdef _OPENMP
-    #pragma omp parallel for collapse(2)
-#endif
-    for (int ip = 0; ip < poolnproc_gps ;++ip)
+    auto unpack_peer = [&](const int ip)
     {
+        const int nzip = numz_gps[ip];
+#ifdef _OPENMP
+        #pragma omp parallel for
+#endif
         for (int is = 0; is < nst_gps; ++is)
         {
-            int nzip = numz_gps[ip];
-            std::complex<T> *outp0 = &out[startz_gps[ip]];
-            std::complex<T> *inp0 = &in[startg_gps[ip]];
-            std::complex<T> *outp = &outp0[is * nz_gps];
-            std::complex<T> *inp = &inp0[is * nzip ];
+            std::complex<T> *outp = &out[is * nz_gps + startz_gps[ip]];
+            std::complex<T> *inp = &in[startg_gps[ip] + is * nzip];
             for (int izip = 0; izip < nzip; ++izip)
             {
                 outp[izip] = inp[izip];
             }
         }
+    };
+
+    ModuleBase::timer::start(this->classname, "gatherp_unpack");
+#ifdef _OPENMP
+    #pragma omp parallel for
+#endif
+    for (int i = 0; i < numg_gps[poolrank_gps]; ++i)
+    {
+        in[startg_gps[poolrank_gps] + i] = sendbuf[startr_gps[poolrank_gps] + i];
     }
+    unpack_peer(poolrank_gps);
     ModuleBase::timer::end(this->classname, "gatherp_unpack");
+
+    while (active_recvs > 0)
+    {
+        int outcount = 0;
+        ModuleBase::timer::start(this->classname, "gatherp_alltoallv");
+        MPI_Waitsome(poolnproc_gps,
+                     recv_requests.data(),
+                     &outcount,
+                     recv_indices.data(),
+                     recv_status.data());
+        ModuleBase::timer::end(this->classname, "gatherp_alltoallv");
+        if (outcount == MPI_UNDEFINED)
+        {
+            break;
+        }
+        for (int idx = 0; idx < outcount; ++idx)
+        {
+            ModuleBase::timer::start(this->classname, "gatherp_unpack");
+            unpack_peer(recv_indices[idx]);
+            ModuleBase::timer::end(this->classname, "gatherp_unpack");
+        }
+        active_recvs -= outcount;
+    }
+
+    if (active_sends > 0)
+    {
+        ModuleBase::timer::start(this->classname, "gatherp_alltoallv");
+        MPI_Waitall(poolnproc_gps, send_requests.data(), MPI_STATUSES_IGNORE);
+        ModuleBase::timer::end(this->classname, "gatherp_alltoallv");
+    }
 #endif
     ModuleBase::timer::end(this->classname, "gatherp_scatters");
     return;
@@ -164,6 +234,11 @@ void PW_Basis::gathers_scatterp(std::complex<T>* in, std::complex<T>* out) const
     const int* numz_ = this->numz;
     const int* startg_ = this->startg;
     const int* startz_ = this->startz;
+    const int* nst_per_ = this->nst_per;
+    const int* startr_ = this->startr;
+    const int poolrank_ = this->poolrank;
+    const int send_count_ = startg_[poolnproc_ - 1] + this->numg[poolnproc_ - 1];
+    std::complex<T>* sendbuf = this->acquire_comm_sendbuf<T>(send_count_);
 #ifdef _OPENMP
     #pragma omp parallel for collapse(2)
 #endif
@@ -172,7 +247,7 @@ void PW_Basis::gathers_scatterp(std::complex<T>* in, std::complex<T>* out) const
         for (int is = 0; is < nst_; ++is)
         {
             int nzip = numz_[ip];
-            std::complex<T> *outp0 = &out[startg_[ip]];
+            std::complex<T> *outp0 = &sendbuf[startg_[ip]];
             std::complex<T> *inp0 = &in[startz_[ip]];
             std::complex<T> *outp = &outp0[is * nzip];
             std::complex<T> *inp = &inp0[is * nz_ ];
@@ -186,18 +261,44 @@ void PW_Basis::gathers_scatterp(std::complex<T>* in, std::complex<T>* out) const
 
     //exchange data
     //(numz[ip],ns, poolnproc) to (nplane,nstot)
-    ModuleBase::timer::start(this->classname, "gathers_alltoallv");
+    MPI_Datatype mpi_type = MPI_DATATYPE_NULL;
     if(typeid(T) == typeid(double))
     {
-        MPI_Alltoallv(out, numg, startg, MPI_DOUBLE_COMPLEX, in, numr, startr, MPI_DOUBLE_COMPLEX, this->pool_world);
+        mpi_type = MPI_DOUBLE_COMPLEX;
     }
     else if(typeid(T) == typeid(float))
     {
-        MPI_Alltoallv(out, numg, startg, MPI_COMPLEX, in, numr, startr, MPI_COMPLEX, this->pool_world);
+        mpi_type = MPI_COMPLEX;
     }
     else
     {
-        ModuleBase::WARNING_QUIT("PW_Basis::gathers_scatterp", "Unsupported data type for MPI_Alltoallv");
+        ModuleBase::WARNING_QUIT("PW_Basis::gathers_scatterp", "Unsupported data type for MPI gather/scatter");
+    }
+    std::vector<MPI_Request> recv_requests(poolnproc_, MPI_REQUEST_NULL);
+    std::vector<MPI_Request> send_requests(poolnproc_, MPI_REQUEST_NULL);
+    std::vector<MPI_Status> recv_status(poolnproc_);
+    std::vector<int> recv_indices(poolnproc_, MPI_UNDEFINED);
+    int active_recvs = 0;
+    int active_sends = 0;
+
+    ModuleBase::timer::start(this->classname, "gathers_alltoallv");
+    for (int ip = 0; ip < poolnproc_; ++ip)
+    {
+        if (ip == poolrank_ || this->numr[ip] == 0)
+        {
+            continue;
+        }
+        MPI_Irecv(&in[startr_[ip]], this->numr[ip], mpi_type, ip, 0, this->pool_world, &recv_requests[ip]);
+        ++active_recvs;
+    }
+    for (int ip = 0; ip < poolnproc_; ++ip)
+    {
+        if (ip == poolrank_ || this->numg[ip] == 0)
+        {
+            continue;
+        }
+        MPI_Isend(&sendbuf[startg_[ip]], this->numg[ip], mpi_type, ip, 0, this->pool_world, &send_requests[ip]);
+        ++active_sends;
     }
     ModuleBase::timer::end(this->classname, "gathers_alltoallv");
 
@@ -213,25 +314,68 @@ void PW_Basis::gathers_scatterp(std::complex<T>* in, std::complex<T>* out) const
     ModuleBase::timer::end(this->classname, "gathers_clear");
 
     //change (nplane,nstot) to (nplane fftnxy)
-    ModuleBase::timer::start(this->classname, "gathers_unpack");
-    const int nstot = this->nstot;
     const int nplane = this->nplane;
     const int* istot2ixy = this->istot2ixy;
-#ifdef _OPENMP
-#pragma omp parallel for
-#endif
-    for (int istot = 0;istot < nstot; ++istot)
+    auto unpack_peer = [&](const int ip)
     {
-        int ixy = istot2ixy[istot];
-        //int ixy = (ixy / fftny)*ny + ixy % fftny;
-        std::complex<T> *outp = &out[ixy * nplane];
-        std::complex<T> *inp = &in[istot * nplane];
-        for (int iz = 0; iz < nplane; ++iz)
+        const int istot0 = startr_[ip] / nplane;
+        const int peer_nst = nst_per_[ip];
+#ifdef _OPENMP
+        #pragma omp parallel for
+#endif
+        for (int is = 0; is < peer_nst; ++is)
         {
-            outp[iz] = inp[iz];
+            const int istot = istot0 + is;
+            const int ixy = istot2ixy[istot];
+            std::complex<T> *outp = &out[ixy * nplane];
+            std::complex<T> *inp = &in[startr_[ip] + is * nplane];
+            for (int iz = 0; iz < nplane; ++iz)
+            {
+                outp[iz] = inp[iz];
+            }
         }
+    };
+
+    ModuleBase::timer::start(this->classname, "gathers_unpack");
+#ifdef _OPENMP
+    #pragma omp parallel for
+#endif
+    for (int i = 0; i < this->numr[poolrank_]; ++i)
+    {
+        in[startr_[poolrank_] + i] = sendbuf[startg_[poolrank_] + i];
     }
+    unpack_peer(poolrank_);
     ModuleBase::timer::end(this->classname, "gathers_unpack");
+
+    while (active_recvs > 0)
+    {
+        int outcount = 0;
+        ModuleBase::timer::start(this->classname, "gathers_alltoallv");
+        MPI_Waitsome(poolnproc_,
+                     recv_requests.data(),
+                     &outcount,
+                     recv_indices.data(),
+                     recv_status.data());
+        ModuleBase::timer::end(this->classname, "gathers_alltoallv");
+        if (outcount == MPI_UNDEFINED)
+        {
+            break;
+        }
+        for (int idx = 0; idx < outcount; ++idx)
+        {
+            ModuleBase::timer::start(this->classname, "gathers_unpack");
+            unpack_peer(recv_indices[idx]);
+            ModuleBase::timer::end(this->classname, "gathers_unpack");
+        }
+        active_recvs -= outcount;
+    }
+
+    if (active_sends > 0)
+    {
+        ModuleBase::timer::start(this->classname, "gathers_alltoallv");
+        MPI_Waitall(poolnproc_, send_requests.data(), MPI_STATUSES_IGNORE);
+        ModuleBase::timer::end(this->classname, "gathers_alltoallv");
+    }
 #endif
     ModuleBase::timer::end(this->classname, "gathers_scatterp");
     return;
